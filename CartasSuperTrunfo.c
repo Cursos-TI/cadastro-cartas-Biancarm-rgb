4f56618c628ce26c78d31dd8efcaf410bdce7d81
Bianca Rangel Moraes <birangelmoraes@gmail.com>
2026-02-10 18:09:45 +0000

    Criei as variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,13 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+    int populacao1, pontos_turisticos1;
+    char estado1, cidade1[50], codigo1[4];
+    float area1, pib1;
+    int populacao2, pontos_turisticos2;
+    char estado2, cidade2[50], codigo2[4];
+    float area2, pib2;
+    
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
